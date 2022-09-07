22496639780849bb30bbd731e2f7aaf3cb06aba8
GrigoriyMay <g-mayorov1996@mail.ru>
2022-09-07 17:23:11 +0300

    Написали программу

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿Console.Clear();
+//Записали метод вывода массива на консоль.
+void PrintArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] +  " ");
+    }
+}
+
+string [] array = {"whd","bwfw", "ebfewe", "fbew", "qw", "12"};
+
+//Определил размерность второго массива
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count = count + 1;
+    }
+}
+string [] mass = new string [count];
+
+//Определил элементы второго массива
+int l = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        mass[l] = array[i];
+        l++;
+    }
+}
+
+PrintArray(array);
+System.Console.WriteLine();
+PrintArray(mass);
